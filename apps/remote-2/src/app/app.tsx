92de1f32218d748cb1d6c89d0940c88ab0d8c642
Oleg Shilov <me@olegshilov.com>
2022-11-18 01:53:30 +0400

    feat: lazy load remote-2feature-bar route

--- apps/remote-2/src/app/app.tsx
+++ apps/remote-2/src/app/app.tsx
@@ -1,14 +1,21 @@
 import { Route, Routes, NavLink } from 'react-router-dom';
-import { Remote2FeatureFoo } from '@nx-playground/remote-2/feature-foo';
 import { Heading } from '@nx-playground/ui';
 import styles from './app.module.css';
 import clsx from 'clsx';
-import { Remote2FeatureBar } from '@nx-playground/remote-2/feature-bar';
+import { Remote2FeatureFoo } from '@nx-playground/remote-2/feature-foo';
+import { lazy, Suspense } from 'react';
+
+const Remote2FeatureBar = lazy(async () => {
+  const { Remote2FeatureBar } = await import(
+    '@nx-playground/remote-2/feature-bar'
+  );
+  return { default: Remote2FeatureBar };
+});
 
 export function App() {
   function getLinkClassName({ isActive }: { isActive: boolean }) {
-    return clsx(styles['link'], {
-      [styles['link-active']]: isActive,
+    return clsx(styles['link-2'], {
+      [styles['link-2-active']]: isActive,
     });
   }
 
@@ -41,38 +48,40 @@ export function App() {
         </ul>
       </div>
 
-      <Routes>
-        <Route path="" element={<Heading level={2}>remote-2 home</Heading>} />
-        <Route
-          path="page-2"
-          element={
-            <div>
-              <Heading level={2}>remote-2 page-2</Heading>
-              <NavLink className={getLinkClassName} to=".." end>
-                Click here to go back to remote-2 home.
-              </NavLink>
-            </div>
-          }
-        />
-        <Route
-          path="/feature-foo"
-          element={
-            <div>
-              <Heading level={2}>remote-2 feature-foo</Heading>
-              <Remote2FeatureFoo />
-            </div>
-          }
-        />
-        <Route
-          path="/feature-bar"
-          element={
-            <div>
-              <Heading level={2}>remote-2 feature-foo</Heading>
-              <Remote2FeatureBar />
-            </div>
-          }
-        />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="" element={<Heading level={2}>remote-2 home</Heading>} />
+          <Route
+            path="page-2"
+            element={
+              <div>
+                <Heading level={2}>remote-2 page-2</Heading>
+                <NavLink className={getLinkClassName} to=".." end>
+                  Click here to go back to remote-2 home.
+                </NavLink>
+              </div>
+            }
+          />
+          <Route
+            path="/feature-foo"
+            element={
+              <div>
+                <Heading level={2}>remote-2 feature-foo</Heading>
+                <Remote2FeatureFoo />
+              </div>
+            }
+          />
+          <Route
+            path="/feature-bar"
+            element={
+              <div>
+                <Heading level={2}>remote-2 feature-foo</Heading>
+                <Remote2FeatureBar />
+              </div>
+            }
+          />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
--- apps/remote-2/src/bootstrap.tsx
+++ apps/remote-2/src/bootstrap.tsx
@@ -1,14 +1,11 @@
 import { ThemeProvider } from '@nx-playground/theme';
 import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
 import { App } from './app/app';
 import './styles.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <StrictMode>
